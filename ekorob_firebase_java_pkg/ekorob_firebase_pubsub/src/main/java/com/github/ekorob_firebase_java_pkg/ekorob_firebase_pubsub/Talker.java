41ebb6b1f39b34703cc7a203890497b98cfdda2f
Gazihan Alankus <gazialankus@gmail.com>
2018-05-15 18:06:02 +0300

    Successfully relays messages to/from ros.

--- ekorob_firebase_java_pkg/ekorob_firebase_pubsub/src/main/java/com/github/ekorob_firebase_java_pkg/ekorob_firebase_pubsub/Talker.java
+++ ekorob_firebase_java_pkg/ekorob_firebase_pubsub/src/main/java/com/github/ekorob_firebase_java_pkg/ekorob_firebase_pubsub/Talker.java
@@ -17,7 +17,9 @@
 package com.github.rosjava.ekorob_firebase_java_pkg.ekorob_firebase_pubsub;
 //package com.github.ekorob_firebase_java_pkg.ekorob_firebase_pubsub;
 
+import com.github.ekorob_firebase_java_pkg.ekorob_firebase_pubsub.ValueBuffer;
 import org.ros.concurrent.CancellableLoop;
+import org.ros.message.MessageListener;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
@@ -34,6 +36,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.auth.oauth2.GoogleCredentials;
+import org.ros.node.topic.Subscriber;
 
 /**
  * A simple {@link Publisher} {@link NodeMain}.
@@ -65,20 +68,14 @@ public class Talker extends AbstractNodeMain {
 
 
     System.out.println("LISTENING LISTENING");
-    
-    DatabaseReference ref = FirebaseDatabase.getInstance()
-        .getReference("location");
-    ref.addValueEventListener(new ValueEventListener() {
-      @Override
-      public void onDataChange(DataSnapshot dataSnapshot) {
-        Object document = dataSnapshot.getValue();
-        System.out.println(document);
-      }
 
-      @Override
-      public void onCancelled(DatabaseError error) {
-      }
-    });
+    //souffle=sufle yok, waitlocation=yes, whoiam=kullanıcı, location=location2, facedetected=no
+
+    startTopic(connectedNode, "souffle");
+    startTopic(connectedNode, "waitlocation");
+    startTopic(connectedNode, "whoiam");
+    startTopic(connectedNode, "location");
+    startTopic(connectedNode, "facedetected");
 
     final Publisher<std_msgs.String> publisher =
         connectedNode.newPublisher("ekorob_firebase", std_msgs.String._TYPE);
@@ -101,5 +98,59 @@ public class Talker extends AbstractNodeMain {
         Thread.sleep(1000);
       }
     });
+  }
+
+  private void startTopic(ConnectedNode connectedNode, final String name) {
+    final ValueBuffer buffer = new ValueBuffer();
+    buffer.value = "";
+
+    java.lang.String topic = "/firebase/" + name;
+    final Publisher<std_msgs.String> pub =
+            connectedNode.newPublisher(topic, std_msgs.String._TYPE);
+    final DatabaseReference ref = FirebaseDatabase.getInstance()
+        .getReference(name);
+    ref.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        Object document = dataSnapshot.getValue();
+        String docStr = document.toString();
+        // publish this
+        System.out.println(name + " from firebase:");
+        System.out.println(docStr);
+
+        if (docStr.equals(buffer.value)) {
+          System.out.println("the same, ignoring");
+        } else {
+          System.out.println("different, sending");
+          std_msgs.String str = pub.newMessage();
+          str.setData(docStr);
+          pub.publish(str);
+          buffer.value = docStr;
+        }
+
+      }
+
+      @Override
+      public void onCancelled(DatabaseError error) {
+      }
+    });
+
+
+    Subscriber<std_msgs.String> subscriber = connectedNode.newSubscriber(topic, std_msgs.String._TYPE);
+    subscriber.addMessageListener(new MessageListener<std_msgs.String>() {
+      @Override
+      public void onNewMessage(std_msgs.String message) {
+        java.lang.String msgData = message.getData();
+
+        System.out.println(name + " to Firebase:");
+        System.out.println(msgData);
+
+        buffer.value = msgData;
+
+        ref.setValueAsync(msgData);
+      }
+    });
+
+
   }
 }
---
+++ ekorob_firebase_java_pkg/ekorob_firebase_pubsub/src/main/java/com/github/ekorob_firebase_java_pkg/ekorob_firebase_pubsub/ValueBuffer.java
@@ -0,0 +1,7 @@
+package com.github.ekorob_firebase_java_pkg.ekorob_firebase_pubsub;
+
+public class ValueBuffer {
+
+    public String value;
+
+}
